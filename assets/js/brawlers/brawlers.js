2cd7c8f3cb01367ab28025429b8131d2d24d5356
RostaJecna <levder@spsejecna.cz>
2023-06-04 23:54:24 +0200

    Add fucntion `displayBrawlers`.

--- assets/js/brawlers/brawlers.js
+++ assets/js/brawlers/brawlers.js
@@ -14,8 +14,62 @@ $.ajax({
         // Sort the brawlers array from higher to lower rarity ID
         brawlers.sort((a, b) => b.brawlerRarity.id - a.brawlerRarity.id);
 
-        console.log(brawlers);
+        displayBrawlers(brawlers);
     },
     error: function (xhr) {
     }
 });
+
+/**
+ * * The `displayBrawlers` function takes an array of `brawlers` as input and dynamically generates HTML elements to display each brawler.
+ *
+ * @param {Array} brawlers - An array of brawlers to be displayed.
+ * @returns {void}
+ */
+function displayBrawlers(brawlers) {
+
+    const brawlersSpinner = document.querySelector('#brawlers-spinner');
+    if (brawlersSpinner) {
+        brawlersSpinner.remove();
+    }
+
+    const brawlersContainer = document.querySelector('#brawlers-container');
+
+    // ? Create a document fragment to hold the generated HTML elements
+    const fragment = document.createDocumentFragment();
+
+    // Loop through the brawlers array and create HTML elements to display them
+    brawlers.forEach(brawler => {
+        const brawlerDiv = document.createElement('div');
+        brawlerDiv.classList.add('col-xl-3', 'col-lg-4', 'col-md-6', 'mb-3', 'brawler-card');
+
+        // Set the innerHTML of the div to display the brawler details
+        brawlerDiv.innerHTML = `
+        <div class="text-center brawler-border brawler-card-inner rounded-4 bg-dark">
+            <div class="brawler-card-front">
+                <div class="py-5 rounded-top-2" style="background-color: ${brawler.brawlerRarity.color};"><img
+                        class="shadow-lg rounded" src="${brawler.iconImageUrl}" alt="brawlerIcon.png" height="152px">
+                </div>
+                <div class="p-3">
+                    <h1 class="m-0 font-nougat" style="color: ${brawler.brawlerRarity.color};">
+                        ${brawler.name}
+                    </h1>
+                    <h3 class="text-muted m-0">
+                        ${brawler.className}
+                    </h3>
+                </div>
+            </div>
+            <div class="brawler-card-back d-flex flex-column justify-content-between py-2">
+                <h1 class="m-0" style="color: ${brawler.brawlerRarity.color};">${brawler.brawlerRarity.name}</h1>
+                <p class="text-muted px-2 m-0">${brawler.description}</p>
+                <img class="mx-auto" src="${brawler.emojiImageUrl}" height="48px" alt="emoji.svg">
+            </div>
+        </div>
+        `;
+
+        // * Append the brawler div to the fragment
+        fragment.appendChild(brawlerDiv);
+    });
+
+    brawlersContainer.appendChild(fragment);
+}
