c2fbf9ff65d0f443896bea29db1bd85528b006d9
RostaJecna <levder@spsejecna.cz>
2023-06-05 21:08:44 +0200

    Optimize code and import new functions.

--- assets/js/brawlers/brawlers.js
+++ assets/js/brawlers/brawlers.js
@@ -1,4 +1,12 @@
 import Brawler from "./brawler.js";
+import { displayArray, displayError } from "../display-manager.js";
+
+const brawlersSpinner = $('#brawlers-spinner');
+const brawlersContainer = $('#brawlers-container');
+const divCardsClasses = ['col-xl-3', 'col-lg-4', 'col-md-6', 'mb-3', 'brawler-card'];
+
+// Hide the element inside brawlers container, toggle the 'd-none' class, and then show it.
+brawlersSpinner.hide().toggleClass('d-none').show(350);
 
 $.ajax({
     url: 'https://api.brawlapi.com/v1/brawlers',
@@ -14,91 +22,34 @@ $.ajax({
         // Sort the brawlers array from higher to lower rarity ID
         brawlers.sort((a, b) => b.brawlerRarity.id - a.brawlerRarity.id);
 
-        displayBrawlers(brawlers);
+        displayArray(brawlersContainer, brawlers, divCardsClasses, generateHtmlBrawler);
     },
     error: function (xhr) {
-        displayError(xhr);
+        displayError(brawlersContainer, xhr);
     }
 });
 
-/**
- * * The `displayBrawlers` function takes an array of `brawlers` as input and dynamically generates HTML elements to display each brawler.
- *
- * @param {Array} brawlers - An array of brawlers to be displayed.
- * @returns {void}
- */
-function displayBrawlers(brawlers) {
-
-    const brawlersSpinner = document.querySelector('#brawlers-spinner');
-    if (brawlersSpinner) {
-        brawlersSpinner.remove();
-    }
-
-    const brawlersContainer = document.querySelector('#brawlers-container');
-
-    // ? Create a document fragment to hold the generated HTML elements
-    const fragment = document.createDocumentFragment();
-
-    // Loop through the brawlers array and create HTML elements to display them
-    brawlers.forEach(brawler => {
-        const brawlerDiv = document.createElement('div');
-        brawlerDiv.classList.add('col-xl-3', 'col-lg-4', 'col-md-6', 'mb-3', 'brawler-card');
-
-        // Set the innerHTML of the div to display the brawler details
-        brawlerDiv.innerHTML = `
-        <div class="text-center brawler-border brawler-card-inner rounded-4 bg-dark">
-            <div class="brawler-card-front">
-                <div class="py-5 rounded-top-2" style="background-color: ${brawler.brawlerRarity.color};"><img
-                        class="shadow-lg rounded" src="${brawler.iconImageUrl}" alt="brawlerIcon.png" height="152px">
-                </div>
-                <div class="p-3">
-                    <h1 class="m-0 font-nougat" style="color: ${brawler.brawlerRarity.color};">
-                        ${brawler.name}
-                    </h1>
-                    <h3 class="text-muted m-0">
-                        ${brawler.className}
-                    </h3>
-                </div>
+function generateHtmlBrawler(brawler) {
+    return `
+    <div class="text-center brawler-border brawler-card-inner rounded-4 bg-dark">
+        <div class="brawler-card-front">
+            <div class="py-5 rounded-top-2" style="background-color: ${brawler.brawlerRarity.color};"><img
+                    class="shadow-lg rounded" src="${brawler.iconImageUrl}" alt="brawlerIcon.png" height="152px">
             </div>
-            <div class="brawler-card-back d-flex flex-column justify-content-between py-2">
-                <h1 class="m-0" style="color: ${brawler.brawlerRarity.color};">${brawler.brawlerRarity.name}</h1>
-                <p class="text-muted px-2 m-0">${brawler.description}</p>
-                <img class="mx-auto" src="${brawler.emojiImageUrl}" height="48px" alt="emoji.svg">
+            <div class="p-3">
+                <h1 class="m-0 font-nougat" style="color: ${brawler.brawlerRarity.color};">
+                    ${brawler.name}
+                </h1>
+                <h3 class="text-muted m-0">
+                    ${brawler.className}
+                </h3>
             </div>
         </div>
-        `;
-
-        // * Append the brawler div to the fragment
-        fragment.appendChild(brawlerDiv);
-    });
-
-    brawlersContainer.appendChild(fragment);
-}
-
-/**
- * * The `displayError` function is responsible for displaying an error message when there is a problem loading data.
- *
- * @param {XMLHttpRequest} xhr - The XMLHttpRequest object containing the error information.
- * @returns {void}
- */
-function displayError(xhr) {
-    const brawlersSpinner = document.querySelector('#brawlers-spinner');
-    if (brawlersSpinner) {
-        brawlersSpinner.remove();
-    }
-
-    const brawlersContainer = document.querySelector('#brawlers-container');
-    const errorDiv = document.createElement('div');
-
-    errorDiv.innerHTML = `
-    <div class="alert alert-danger" role="alert" data-bs-theme="light">
-    Apologies for the inconvenience. We're currently unable to load the data. Please try
-        <span><a href="/pages/brawlers.html" class="link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">again</a></span> in a few minutes.
-        <hr>
-        Status Code: ${xhr.status}.
-        Message: ${xhr.statusText}.
+        <div class="brawler-card-back d-flex flex-column justify-content-between py-2">
+            <h1 class="m-0" style="color: ${brawler.brawlerRarity.color};">${brawler.brawlerRarity.name}</h1>
+            <p class="text-muted px-2 m-0">${brawler.description}</p>
+            <img class="mx-auto" src="${brawler.emojiImageUrl}" height="48px" alt="emoji.svg">
+        </div>
     </div>
     `;
-
-    brawlersContainer.appendChild(errorDiv);
 }
--- assets/js/index/index.js
+++ assets/js/index/index.js
@@ -1,10 +1,18 @@
 import GameEvent from "./game-event.js";
+import { displayArray, displayError } from "../display-manager.js";
+
+const gameEventsSpinner = $('#game-events-spinner');
+const gameEventsContainer = $('#game-events-container');
+const divEventsClasses = ['col-lg-6', 'col-sm-12', 'd-flex', 'flex-column', 'mb-3'];
+
+// Hide the element inside events container, toggle the 'd-none' class, and then show it.
+gameEventsSpinner.hide().toggleClass('d-none').show(350);
 
 $.ajax({
     url: 'https://api.brawlapi.com/v1/events',
     method: 'GET',
     dataType: 'json',
-    success: function (response) {
+    success: (response) => {
         // ? Access the 'active' array from the JSON data
         const activeEvents = response.active;
 
@@ -15,87 +23,30 @@ $.ajax({
         });
 
         // * Create an array of GameEvent objects from the filtered events
-        const gameEvents = filteredEvents.map(event => GameEvent.getFiltredEvent(event));
+        const gameEventsArray = filteredEvents.map(event => GameEvent.getFiltredEvent(event));
 
-        displayEvents(gameEvents);
+        displayArray(gameEventsContainer, gameEventsArray, divEventsClasses, generateHtmlEvent);
     },
-    error: function (xhr) {
-        displayError(xhr);
+    error: (xhr) => {
+        displayError(gameEventsContainer, xhr);
     }
 });
 
-/**
- * * The `displayEvents` function takes an array of `gameEvents` as input and dynamically generates HTML elements to display each game event.
- *
- * @param {Array} gameEvents - An array of game events to be displayed.
- * @returns {void}
- */
-function displayEvents(gameEvents) {
-
-    const gameEventsSpinner = document.querySelector('#game-events-spinner');
-    if (gameEventsSpinner) {
-        gameEventsSpinner.remove();
-    }
-
-    const gameEventsContainer = document.querySelector('#game-events-container');
-
-    // ? Create a document fragment to hold the generated HTML elements
-    const fragment = document.createDocumentFragment();
-
-    // Loop through the game events array and create HTML elements to display them
-    gameEvents.forEach(event => {
-        const eventDiv = document.createElement('div');
-        eventDiv.classList.add('col-lg-6', 'col-sm-12', 'd-flex', 'flex-column', 'mb-3');
-
-        // Set the innerHTML of the div to display the game event details
-        eventDiv.innerHTML = `
-        <div class="bg-black event-time-info rounded-top">
-            <p class="text-end fs-5 p-1 me-1 mb-0">New Event in:
-                <span>${event.getRemainingTime()}</span>
-            </p>
-        </div>
-        <div class="d-flex p-2 event-header align-items-center" style="background-color: ${event.gameMode.bgColor};">
-            <img class="user-select-none" src="${event.gameMode.iconImageUrl}" alt="icon.png" height="72">
-            <div class="px-2 text-white event-shadow-text ">
-                <h2 class="text-uppercase mb-0">${event.gameMode.name}</h2>
-                <h4 class="text-capitalize mb-0">${event.gameMap.name}</h4>
-            </div>
-        </div>
-        <div class="event-bg-image rounded-bottom"
-            style="background-image: url(${event.gameMap.bgImageUrl});"></div>
-        `;
-
-        // * Append the event div to the fragment
-        fragment.appendChild(eventDiv);
-    });
-
-    gameEventsContainer.appendChild(fragment);
-}
-
-/**
- * * The `displayError` function is responsible for displaying an error message when there is a problem loading data.
- *
- * @param {XMLHttpRequest} xhr - The XMLHttpRequest object containing the error information.
- * @returns {void}
- */
-function displayError(xhr) {
-    const gameEventsSpinner = document.getElementById('game-events-spinner');
-    if (gameEventsSpinner) {
-        gameEventsSpinner.remove();
-    }
-
-    const gameEventsContainer = document.getElementById('game-events-container');
-    const errorDiv = document.createElement('div');
-
-    errorDiv.innerHTML = `
-    <div class="alert alert-danger" role="alert" data-bs-theme="light">
-    Apologies for the inconvenience. We're currently unable to load the data. Please try
-        <span><a href="/" class="link-warning link-offset-2 link-underline-opacity-25 link-underline-opacity-100-hover">again</a></span> in a few minutes.
-        <hr>
-        Status Code: ${xhr.status}.
-        Message: ${xhr.statusText}.
+function generateHtmlEvent(event) {
+    return `
+    <div class="bg-black event-time-info rounded-top">
+        <p class="text-end fs-5 p-1 me-1 mb-0">New Event in:
+            <span>${event.getRemainingTime()}</span>
+        </p>
     </div>
+    <div class="d-flex p-2 event-header align-items-center" style="background-color: ${event.gameMode.bgColor};">
+        <img class="user-select-none" src="${event.gameMode.iconImageUrl}" alt="icon.png" height="72">
+        <div class="px-2 text-white event-shadow-text ">
+            <h2 class="text-uppercase mb-0">${event.gameMode.name}</h2>
+            <h4 class="text-capitalize mb-0">${event.gameMap.name}</h4>
+        </div>
+    </div>
+    <div class="event-bg-image rounded-bottom"
+        style="background-image: url(${event.gameMap.bgImageUrl});"></div>
     `;
-
-    gameEventsContainer.appendChild(errorDiv);
 }
