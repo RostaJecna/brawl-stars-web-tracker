f7d0e79ee06d1fca696786e9bf6fd9434f4018a5
RostaJecna <levder@spsejecna.cz>
2023-06-06 22:02:14 +0200

    Upload `404.php` on GitHub.

---
+++ pages/404.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <link rel="stylesheet" href="/assets/css/fonts.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+        <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
+        <title>Brawl Tracker | 404</title>
+    </head>
+
+    <body>
+        <main class="container">
+            <div class="row mt-5 justify-content-center p-5 user-select-none">
+                <div class="col-md-4 my-auto text-center">
+                    <img
+                        src="/assets/images/icons/404-gray.webp"
+                        height="172px"
+                        alt="404-brawler-icon"
+                    />
+                </div>
+                <div class="col-md-6">
+                    <h1 class="font-nougat text-warning">Uh-oh!</h1>
+                    <h2 class="font-lilita-one text-secondary">
+                        Error 404: Page not found!
+                    </h2>
+                    <h4 class="font-nanum-gothic">
+                        It seems we've stumbled upon a Brawl Breaker. Maybe they
+                        accidentally tumbled into a hidden bush or got caught up
+                        in a wild Super attack. Time to gather your team and
+                        start the search party! Don't worry, we'll make sure
+                        they're back in action before you can say 'Poco's
+                        Encore!
+                    </h4>
+                </div>
+            </div>
+        </main>
+    </body>
+</html>
