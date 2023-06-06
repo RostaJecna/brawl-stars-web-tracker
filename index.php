528edde9792c51d0400fa1ec60d60bd8c53e6cf2
RostaJecna <levder@spsejecna.cz>
2023-06-06 18:34:22 +0200

    Change index from `.html` to `.php`.

--- index.php
+++ index.php
@@ -22,15 +22,13 @@
                     <img class="bi" width="32" height="32" alt="logo.svg" src="/assets/images/logo.svg">
                     <span class="fs-4 p-2 font-nougat">Brawl Tracker</span>
                 </a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01"
-                    aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarColor01">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-link">
-                            <a class="link-underline-warning link-offset-2 text-white" aria-current="page"
-                                href="/">Home</a>
+                            <a class="link-underline-warning link-offset-2 text-white" aria-current="page" href="/">Home</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="/pages/brawlers.html">Brawlers</a>
@@ -40,11 +38,9 @@
                         </li>
                     </ul>
                     <div class="d-flex">
-                        <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal"
-                            class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover me-3 my-auto">Login
+                        <a href="#" data-bs-toggle="modal" data-bs-target="#loginModal" class="link-body-emphasis link-offset-2 link-underline-opacity-25 link-underline-opacity-75-hover me-3 my-auto">Login
                         </a>
-                        <a class="btn btn-outline-warning action-button" role="button" href="#" data-bs-toggle="modal"
-                            data-bs-target="#signupModal">Sign Up</a>
+                        <a class="btn btn-outline-warning action-button" role="button" href="#" data-bs-toggle="modal" data-bs-target="#signupModal">Sign Up</a>
                     </div>
                 </div>
             </div>
@@ -66,21 +62,16 @@
                             <label for="player-tag-input" class="form-label">Try It Out</label>
                             <div class="input-group">
                                 <span class="input-group-text text-bg-warning" id="basic-addon3">#</span>
-                                <input type="text"
-                                    class="form-control focus-ring focus-ring-warning focus-border-warning"
-                                    id="player-tag-input" placeholder="Player Tag" aria-label="Player Tag"
-                                    aria-describedby="basic-addon4" required>
+                                <input type="text" class="form-control focus-ring focus-ring-warning focus-border-warning" id="player-tag-input" placeholder="Player Tag" aria-label="Player Tag" aria-describedby="basic-addon4" required>
                                 <button class="btn btn-outline-warning" type="submit" id="button-addon2">Get</button>
                             </div>
                             <div class="form-text" id="basic-addon4">
-                                <a href="#"
-                                    class="link-body-emphasis link-offset-2 link-opacity-75 link-underline-opacity-0 link-underline-opacity-100-hover me-3">Where
+                                <a href="#" class="link-body-emphasis link-offset-2 link-opacity-75 link-underline-opacity-0 link-underline-opacity-100-hover me-3">Where
                                     can I find my tag?</a>
                             </div>
                         </form>
                     </div>
-                    <div
-                        class="col-md-7 my-4 my-md-0 d-flex justify-content-center align-items-center user-select-none">
+                    <div class="col-md-7 my-4 my-md-0 d-flex justify-content-center align-items-center user-select-none">
                         <div class="text-center text-uppercase text-body-tertiary">
                             <h1 class="m-0">Your profile</h1>
                             <h5 class="m-0">Will be displayed here</h5>
@@ -109,16 +100,14 @@
                 </div>
 
                 <div class="col-md-4 justify-content-end d-flex me-2 mb-3 mb-md-0">
-                    <a href="https://github.com/RostaJecna"><img class="bi" width="32" height="32"
-                            src="/assets/images/icons/github.svg" alt="github.svg"></a>
+                    <a href="https://github.com/RostaJecna"><img class="bi" width="32" height="32" src="/assets/images/icons/github.svg" alt="github.svg"></a>
                 </div>
             </div>
         </div>
     </footer>
 
     <!-- Login Modal -->
-    <div class="modal fade user-select-none" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel"
-        aria-hidden="true">
+    <div class="modal fade user-select-none" id="loginModal" tabindex="-1" aria-labelledby="loginModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -129,15 +118,11 @@
                     <form id="loginForm">
                         <div class="mb-3">
                             <label for="login-email" class="form-label">Email</label>
-                            <input type="email"
-                                class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic"
-                                id="login-email" name="login-email" placeholder="Enter your email" required>
+                            <input type="email" class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic" id="login-email" name="login-email" placeholder="Enter your email" required>
                         </div>
                         <div class="mb-4">
                             <label for="login-password" class="form-label">Password</label>
-                            <input type="password"
-                                class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic"
-                                id="login-password" name="login-password" placeholder="Enter your password" required>
+                            <input type="password" class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic" id="login-password" name="login-password" placeholder="Enter your password" required>
                         </div>
                         <div class="modal-footer">
                             <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
@@ -150,8 +135,7 @@
     </div>
 
     <!-- Signup Modal -->
-    <div class="modal fade user-select-none" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel"
-        aria-hidden="true">
+    <div class="modal fade user-select-none" id="signupModal" tabindex="-1" aria-labelledby="signupModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -162,29 +146,21 @@
                     <form id="signupForm">
                         <div class="mb-3">
                             <label for="signup-email" class="form-label">Email</label>
-                            <input type="email"
-                                class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic"
-                                id="signup-email" name="signup-email" placeholder="Enter your email" required>
+                            <input type="email" class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic" id="signup-email" name="signup-email" placeholder="Enter your email" required>
                         </div>
                         <div class="mb-3">
                             <label for="signup-password" class="form-label">Password</label>
-                            <input type="password"
-                                class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic"
-                                id="signup-password" name="signup-password" placeholder="Enter your password" required>
+                            <input type="password" class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic" id="signup-password" name="signup-password" placeholder="Enter your password" required>
                         </div>
                         <div class="mb-2">
                             <label for="signup-player-tag" class="form-label">Player Tag</label>
                             <div class="input-group">
                                 <span class="input-group-text text-bg-warning">#</span>
-                                <input type="text"
-                                    class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic"
-                                    id="signup-player-tag" name="signup-player-tag" placeholder="Enter your player tag"
-                                    required>
+                                <input type="text" class="form-control focus-ring focus-ring-warning focus-border-warning font-nanum-gothic" id="signup-player-tag" name="signup-player-tag" placeholder="Enter your player tag" required>
                             </div>
                         </div>
                         <div class="form-text mb-2">
-                            <a href="#"
-                                class="link-body-emphasis link-offset-2 link-opacity-75 link-underline-opacity-0 link-underline-opacity-100-hover me-3">Where
+                            <a href="#" class="link-body-emphasis link-offset-2 link-opacity-75 link-underline-opacity-0 link-underline-opacity-100-hover me-3">Where
                                 can I find my tag?</a>
                         </div>
                         <div class="modal-footer">
