a1b2f8f65c1e9c8e53c5573e0f1734112225e1af
RostaJecna <levder@spsejecna.cz>
2023-06-08 08:02:11 +0200

    Add favicon for `404` page.

--- pages/404.php
+++ pages/404.php
@@ -5,6 +5,7 @@
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <link rel="stylesheet" href="/assets/css/fonts.css" />
+    <link rel="shortcut icon" href="/assets/images/logo.svg" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
     <script src="/vendor/bootstrap/js/bootstrap.min.js"></script>
